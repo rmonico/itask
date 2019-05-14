37347e6404f21412914de83e14b1591048db2b97
Rafael Monico <rmonico@moninburgo>
2019-05-14 11:58:38 -0300

    Removed deploy script, sources packaged to be deployed via pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='iTask',
+      version='0.1',
+      description='A CLI frontend to easier the use of Taskwarrior',
+      url='https://github.com/rmonico/itask',
+      author='Rafael Monico',
+      author_email='rmonico1@gmail.com',
+      license='GPL3',
+      packages=['itask'],
+      entry_points = {
+          'console_scripts': ['itask=itask.itask'],
+      },
+      zip_safe=False)
