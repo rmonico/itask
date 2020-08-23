b05765fee386943ea4a054a9c044dd2a8501de42
Rafael Monico <rmonico@moninburgo>
2020-08-23 02:36:30 -0300

    Wrapper for SIGWINCH (terminal resize unix signal)

---
+++ itask/terminal_resize_handler.py
@@ -0,0 +1,25 @@
+class TerminalResizeListener(object):
+
+    _instance = None
+
+    def __new__(cls):
+        if TerminalResizeListener._instance is None:
+            TerminalResizeListener._instance = object.__new__(cls)
+
+        return TerminalResizeListener._instance
+
+    def __init__(self):
+        self._listeners = list()
+
+        signal.signal(signal.SIGWINCH, self._handler)
+
+    def _handler(self, signum, stack):
+        for listener in self._listeners:
+            listener()
+
+    def register_listener(self, handler):
+        self._listeners.append(handler)
+
+    def unregister_listener(self, handler):
+        self._listeners.remove(handler)
+
