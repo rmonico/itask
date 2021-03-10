25ae9614188719b0fad87065dbeb7371391eba2a
Rafael Monico <rmonico@moningrado>
2021-03-10 15:10:09 -0300

    Version updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='iTask',
-      version='0.3.3',
+      version='0.3.4',
       description='A CLI frontend to easier the use of Taskwarrior',
       url='https://github.com/rmonico/itask',
       author='Rafael Monico',
